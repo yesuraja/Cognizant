d6f17823fae26116458bc1f6ce08c002bc7f9137
197188 <197188@LTIN237421.cts.com>
2020-11-25 10:51:57 +0530

    new

---
+++ src/test/java/com/test/qa/pageFactory/LaunchPageFactory.java
@@ -0,0 +1,29 @@
+package com.test.qa.pageFactory;
+
+
+import com.test.qa.libraries.FunctionalLibrary;
+import com.test.qa.utilities.ExcelUtil;
+
+import org.junit.Assert;
+
+
+public class LaunchPageFactory extends FunctionalLibrary {
+	ExcelUtil excelUtil=new ExcelUtil();
+
+	
+public void getTestCaseName( String sheetName, String testCaseName) throws Exception {
+	testcase_sheetName= sheetName;
+	testcase_rowNumber = excelUtil.getRowNum(sheetName,testCaseName);
+	if(testcase_rowNumber!=0)
+	{
+		System.out.println("Test Case Row Number: " + testcase_rowNumber);
+	}
+	else
+	{
+		Assert.fail("Test case Name not found in the Excel sheet");
+		extentreportmanager.tearReport();
+		quitDriver();
+	}
+	
+}
+}
