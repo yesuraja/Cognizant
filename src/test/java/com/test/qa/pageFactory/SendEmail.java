895c844e0e6c5330f5217f1dabac5be1f42f60d3
197188 <197188@LTIN237421.cts.com>
2020-11-25 10:13:27 +0530

    new commit

---
+++ src/test/java/com/test/qa/pageFactory/SendEmail.java
@@ -0,0 +1,15 @@
+package com.test.qa.pageFactory;
+
+public class SendEmail {
+/*	
+	SendEmail email = new SendEmail();
+	email.setHostName("smtp.googlemail.com");
+	email.setSmtpPort(465);
+	email.setAuthenticator(new DefaultAuthenticator("username", "password"));
+	email.setSSLOnConnect(true);
+	email.setFrom("yesuraja.pitchaiyan@cognizant.com");
+	email.setSubject("Physical Security Execution Result");
+	email.setMsg("This is a test mail ... :-)");
+	email.addTo("foo@bar.com");
+	email.send(); */
+}
